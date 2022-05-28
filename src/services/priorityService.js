1d77656d0f18c2c6304dd16eac32cf717cf9cd5b
volkan karaali <volkankaraali1@gmail.com>
2022-05-28 14:56:15 +0300

    server link added

--- src/services/priorityService.js
+++ src/services/priorityService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl='http://localhost:5000/';
+const baseUrl='https://personal-job-tracking-server.herokuapp.com/';
 
 export const getPriorites = async ()=>{
   try {
